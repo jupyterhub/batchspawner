986603950fc49833a64ae87391caa48197526e12
Michael Milligan <mmilligan@astro.umn.edu>
2018-04-01 19:26:33 -0500

    set versions for release candidate

--- version.py
+++ version.py
@@ -3,11 +3,9 @@
 
 version_info = (
     0,
+    8,
     0,
-    1,
-    'dev', # comment-out this line for a release
+    'rc0', # comment-out this line for a release
 )
-__version__ = '.'.join(map(str, version_info[:3]))
+__version__ = '.'.join(map(str, version_info))
 
-if len(version_info) > 3:
-    __version__ = '%s-%s' % (__version__, version_info[3])
