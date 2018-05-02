cb053329090c8c50de9f6273221f8808295a99c8
M Milligan <milligan@umn.edu>
2018-05-02 14:57:46 -0500

    Remove dev version tag for release

--- version.py
+++ version.py
@@ -5,7 +5,7 @@ version_info = (
     0,
     8,
     1,
-    'dev', # comment-out this line for a release
+#    'dev', # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info))
 
