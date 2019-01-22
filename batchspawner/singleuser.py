b8638d4e64ef6f1a9fa61f172df2f3f37a34de75
Michael James Jamie  Schnaitter <mschnait@ist.ucf.edu>
2019-01-22 12:09:22 -0500

    added support for hub-aware JupyterLab spawning

--- batchspawner/singleuser.py
+++ batchspawner/singleuser.py
@@ -1,6 +1,7 @@
 from jupyterhub.singleuser import SingleUserNotebookApp
 from jupyterhub.utils import random_port, url_path_join
 from traitlets import default
+import sys
 
 class BatchSingleUserNotebookApp(SingleUserNotebookApp):
     @default('port')
@@ -14,8 +15,43 @@ class BatchSingleUserNotebookApp(SingleUserNotebookApp):
                                    json={'port' : self.port})
         super().start()
 
+try:
+    from jupyterlab.labapp import LabApp
+except ImportError:
+    BatchSingleUserLabApp = None
+    pass
+else:
+    class BatchSingleUserLabApp(BatchSingleUserNotebookApp, LabApp):
+
+        @default
+        def _default_url(self):
+            """when using jupyter-labhub, jupyterlab is default ui"""
+            return "/lab"
+
+        def init_webapp(self, *args, **kwargs):
+            super().init_webapp(*args, **kwargs)
+            settings = self.web_app.settings
+            if 'page_config_data' not in settings:
+                settings['page_config_data'] = {}
+            settings['page_config_data']['hub_prefix'] = self.hub_prefix
+            settings['page_config_data']['hub_host'] = self.hub_host
+            settings['page_config_data']['hub_user'] = self.user
+            api_token = os.getenv('JUPYTERHUB_API_TOKEN')
+            if not api_token:
+                api_token = ''
+            if not self.token:
+                try:
+                    self.token = api_token
+                except AttributeError:
+                    self.log.error("Can't set self.token")
+            settings['page_config_data']['token'] = api_token
+
 def main(argv=None):
-    return BatchSingleUserNotebookApp.launch_instance(argv)
+    if len(argv) > 1 and argv[1] == "lab" and BatchSingleUserLabApp != None:
+        del argv[1]
+        return BatchSingleUserLabApp.launch_instance(argv)
+    else:
+        return BatchSingleUserNotebookApp.launch_instance(argv)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
