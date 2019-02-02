49fe2e82737efeea00b097753c50cf818dbb1c47
Richard Darst <rkd@zgib.net>
2019-02-03 00:51:42 +0200

    Bugfix: Jupyterhub master (beyond 0.9.4) makes get_current_user coroutine

--- batchspawner/api.py
+++ batchspawner/api.py
@@ -6,7 +6,12 @@ class BatchSpawnerAPIHandler(APIHandler):
     @web.authenticated
     def post(self):
         """POST set user's spawner port number"""
-        user = self.get_current_user()
+        if hasattr(self, 'current_user'):
+            # Jupyterhub compatability, (september 2018, d79a99323ef1d)
+            user = self.current_user
+        else:
+            # Previous jupyterhub, 0.9.4 and before.
+            user = self.get_current_user()
         data = self.get_json_body()
         port = int(data.get('port', 0))
         user.spawner.current_port = port
