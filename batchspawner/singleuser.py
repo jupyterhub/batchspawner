a5e55bb5eea1e1d2153075d700ba0689268d47b1
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-04-24 16:28:56 -0400

    Allow batchspawner to be used with different notebook app
    Instead of creating a batchspawner notebook app, batchspawner-singleuser
    is now a wrapper that finds a port and add the port number to the
    command-line argument of the singleuser app. This allows the usage
    of batchspawner with jupyterlab for example.

--- batchspawner/singleuser.py
+++ batchspawner/singleuser.py
@@ -1,21 +1,21 @@
-from jupyterhub.singleuser import SingleUserNotebookApp
+import sys
+
+from runpy import run_path
+from shutil import which
+
 from jupyterhub.utils import random_port, url_path_join
-from traitlets import default
-
-class BatchSingleUserNotebookApp(SingleUserNotebookApp):
-    @default('port')
-    def _port(self):
-        return random_port()
-
-    def start(self):
-        # Send Notebook app's port number to remote Spawner
-        self.hub_auth._api_request(method='POST',
-                                   url=url_path_join(self.hub_api_url, 'batchspawner'),
-                                   json={'port' : self.port})
-        super().start()
+from jupyterhub.services.auth import HubAuth
 
 def main(argv=None):
-    return BatchSingleUserNotebookApp.launch_instance(argv)
+    port = random_port()
+    hub_auth = HubAuth()
+    hub_auth._api_request(method='POST',
+                          url=url_path_join(hub_auth.api_url, 'batchspawner'),
+                          json={'port' : port})
+
+    cmd_path = which(sys.argv[1])
+    sys.argv = sys.argv[1:] + ['--port={}'.format(port)]
+    run_path(cmd_path, run_name="__main__")
 
 if __name__ == "__main__":
     main()
