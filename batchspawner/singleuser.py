328a306168dda3176e797b38a2e9fce759a1c34a
mahendrapaipuri <mahendra.paipuri@gmail.com>
2022-11-24 14:14:10 +0100

    Upgrade singleuser to work with JH 3
    API request method of HubAuth in JupyterHub 3 changed to coroutine
    
    Singleuser script is modified appropriately to make it work with JupyterHub 3

--- batchspawner/singleuser.py
+++ batchspawner/singleuser.py
@@ -1,11 +1,16 @@
 import os
 import sys
+import json
+import asyncio
 
 from runpy import run_path
 from shutil import which
 
+import jupyterhub
 from jupyterhub.utils import random_port, url_path_join
 from jupyterhub.services.auth import HubAuth
+
+JH_MAJOR_VERSION, _, _ = jupyterhub.__version__.split('.')
 
 
 def main(argv=None):
@@ -14,11 +19,24 @@ def main(argv=None):
     hub_auth.client_ca = os.environ.get("JUPYTERHUB_SSL_CLIENT_CA", "")
     hub_auth.certfile = os.environ.get("JUPYTERHUB_SSL_CERTFILE", "")
     hub_auth.keyfile = os.environ.get("JUPYTERHUB_SSL_KEYFILE", "")
-    hub_auth._api_request(
-        method="POST",
-        url=url_path_join(hub_auth.api_url, "batchspawner"),
-        json={"port": port},
-    )
+    if int(JH_MAJOR_VERSION) < 3:
+        hub_auth._api_request(
+            method="POST",
+            url=url_path_join(hub_auth.api_url, "batchspawner"),
+            json={"port": port},
+        )
+    else:
+        # Starting from JupyterHub 3.0.0, the method _api_request is
+        # asynchronous. Consequently the request is made using ASyncHTTPClient
+        # and hence json argument is no longer valid. We need to change it to
+        # body and encode it.
+        asyncio.run(
+            hub_auth._api_request(
+                method="POST",
+                url=url_path_join(hub_auth.api_url, "batchspawner"),
+                body=json.dumps({"port": port}),
+            )
+        )
 
     cmd_path = which(sys.argv[1])
     sys.argv = sys.argv[1:] + ["--port={}".format(port)]
