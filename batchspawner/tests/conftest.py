5ed2e1de32b7c4e7ca1b396a319e750e5293bdf8
Thomas Mendoza <thomas@hitotsu.me>
2024-11-30 21:57:18 -0800

    Remove test orm depenedency and drop event_loop

--- batchspawner/tests/conftest.py
+++ batchspawner/tests/conftest.py
@@ -1,4 +1,1 @@
 """Relevant pytest fixtures are re-used from JupyterHub's test suite"""
-
-# We use "db" directly, but we also need event_loop
-from jupyterhub.tests.conftest import db, event_loop  # noqa
