a0004aab636255c8a7f6c096d9234ed1aca89056
Muhamed Fazal PS <muhamedfazalps@gmail.com>
2026-07-18 18:58:11 +0530

    fix: update pyupgrade hook to v3.21.2 and add try-except fallback for jupyterhub conftest event_loop import

--- batchspawner/tests/conftest.py
+++ batchspawner/tests/conftest.py
@@ -1,4 +1,6 @@
 """Relevant pytest fixtures are re-used from JupyterHub's test suite"""
 
-# We use "db" directly, but we also need event_loop
-from jupyterhub.tests.conftest import db, event_loop  # noqa
+try:
+    from jupyterhub.tests.conftest import db, event_loop  # noqa
+except ImportError:
+    from jupyterhub.tests.conftest import db  # noqa
